b7346c27fcff0512c5c23cfef4133a90ac2a55e7
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2018-11-27 00:27:59 -0600

    Se volvio agregar lo de herna, cuando hice un push se borro lo de el, pero ya qeudo nuevamente todo junto

--- Punto de Venta/src/Script_BD/Script Punto de Venta.sql
+++ Punto de Venta/src/Script_BD/Script Punto de Venta.sql
@@ -599,6 +599,20 @@ call getBusquedaCategoria3(2,'','');
  
  */
  
+ DROP PROCEDURE IF EXISTS getBusquedaCategoria4;/*RECOMIENDO USAr la opcion 3*/
+CREATE PROCEDURE getBusquedaCategoria4(
+id_cat int,  
+nombre VARCHAR(200), 
+descripcion VARCHAR(300))
+	SELECT * from Categoria AS a
+	where a.id_categoria like(CONCAT('%',id_cat,'%'))
+    AND a.nombre like (CONCAT('%',nombre,'%'))
+	AND  a.descripcion like (CONCAT('%',descripcion,'%'))
+    AND  status='activo';
+;
+
+
+ 
   /***********************************************************Procedimientos de MARCA*/
 /*
 id_marca int not null primary key auto_increment,
@@ -1260,3 +1274,108 @@ call getBusquedaProducto4('','lla','');
 	
 */
 
+
+
+
+/***********************************************************Procedimientos de Sucursal*/
+SELECT * FROM Sucursal;
+DROP PROCEDURE IF EXISTS getSucursales;
+CREATE PROCEDURE getSucursales()
+	SELECT * FROM Sucursal WHERE status='activo';
+    
+
+
+DROP PROCEDURE IF EXISTS addSucursal;
+CREATE PROCEDURE addSucursal(
+nombre varchar(100),
+sucursal varchar(50),
+telefono varchar(10),
+correo varchar(100),
+direccion varchar(100),
+colonia varchar(100),
+municipio varchar(200),
+cp varchar(5),
+estado varchar(200))
+INSERT INTO Sucursal (nombre,sucursal,telefono,correo,direccion,colonia,municipio,cp,estado,status) 
+				VALUES(nombre,sucursal,telefono,correo,direccion,colonia,municipio,cp,estado,'activo');
+                
+DROP PROCEDURE IF EXISTS updateSucursal;
+CREATE PROCEDURE updateSucursal(
+id_sucursal int,
+nombre varchar(100),
+sucursal varchar(50),
+telefono varchar(10),
+correo varchar(100),
+direccion varchar(100),
+colonia varchar(100),
+municipio varchar(200),
+cp varchar(5),
+estado varchar(200))
+UPDATE Sucursal as s
+	SET 
+		s.nombre=nombre,
+        s.sucursal=sucursal,
+		s.telefono=telefono,
+		s.correo=correo,
+		s.direccion=direccion,
+		s.colonia=colonia,
+		s.municipio=municipio,
+		s.cp=cp,
+		s.estado=estado
+WHERE s.id_sucursal = id_sucursal;
+
+
+DROP PROCEDURE IF EXISTS deleteSucursal;
+CREATE PROCEDURE deleteSucursal(
+id_sucursal int)
+UPDATE Sucursal as s 
+	SET 
+		s.status='inactivo'
+WHERE s.id_sucursal = id_sucursal;
+  
+DROP PROCEDURE IF EXISTS getBusquedaSucursal1;
+CREATE PROCEDURE getBusquedaSucursal1(  
+nombre varchar(100),
+sucursal varchar(50),
+telefono varchar(10),
+correo varchar(100),
+direccion varchar(100),
+colonia varchar(100),
+municipio varchar(200),
+cp varchar(5),
+estado varchar(200))
+	SELECT * from Sucursal AS s
+	where  s.nombre like (CONCAT('%',nombre,'%'))
+    AND s.sucursal like (CONCAT('%',sucursal,'%'))
+    AND s.telefono like (CONCAT('%',telefono,'%'))
+    AND s.correo like (CONCAT('%',correo,'%'))
+    AND s.direccion like (CONCAT('%',direccion,'%'))
+    AND s.colonia like (CONCAT('%',colonia,'%'))
+    AND s.municipio like (CONCAT('%',municipio,'%'))
+    AND s.cp like (CONCAT('%',cp,'%'))
+    AND s.estado like (CONCAT('%',estado,'%'))
+    AND status = 'activo'
+;
+DROP PROCEDURE IF EXISTS getBusquedaSucursal2;
+CREATE PROCEDURE getBusquedaSucursal2( 
+id_sucursal varchar(100),
+nombre varchar(100),
+sucursal varchar(50),
+telefono varchar(10),
+correo varchar(100),
+direccion varchar(100),
+colonia varchar(100),
+municipio varchar(200),
+cp varchar(5),
+estado varchar(200))
+	SELECT * from Sucursal AS s
+	where CONVERT(s.id_sucursal,CHAR)  like (CONCAT('%',id_sucursal,'%'))
+    AND s.nombre like (CONCAT('%',nombre,'%'))
+	AND s.telefono like (CONCAT('%',telefono,'%'))
+	AND s.correo like (CONCAT('%',correo,'%'))/*Uso convert para comparar el valor char recibido con un int o double*/
+    AND s.direccion  like (CONCAT('%',direccion,'%'))
+	AND s.colonia like (CONCAT('%',colonia,'%'))
+    AND s.municipio like (CONCAT('%',municipio,'%'))
+	AND s.cp like (CONCAT('%',cp,'%'))
+    AND s.estado like (CONCAT('%',estado,'%'))
+    AND status='activo';
